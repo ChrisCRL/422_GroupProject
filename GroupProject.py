167df8dea1613b01ab694b23be9d8d4ca253065c
Ethan Z <161177620+EthanZ23@users.noreply.github.com>
2024-04-04 10:42:54 -0700

    added the python file
    jus a file

---
+++ GroupProject.py
@@ -0,0 +1,8 @@
+"""
+ * CS 422 - 1002, Final Group Project
+ * Group Member Names: Charles Ballesteros, Christopher Liscano, and
+ *                     Ethan Zambrano
+ * Input: .data file: student_spending.csv
+ * Output: Table showing coefficients of the seven independent variables
+ *         and RMSE, for each fold (10-fold).
+"""
