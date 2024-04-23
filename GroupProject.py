66a98f59122ee8c73fb1a17c5acba33f4af96f1b
Ethan Z <161177620+EthanZ23@users.noreply.github.com>
2024-04-23 11:09:20 -0700

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -2,7 +2,7 @@
  * CS 422 - 1002, Final Group Project
  * Group Member Names: Charles Ballesteros, Christopher Liscano, and
  *                     Ethan Zambrano
- * Input: .data file: student_spending.csv
+ * Input: .csv file: student_spending.csv
  * Output: Table showing coefficients of the seven independent variables
  *         and RMSE, for each fold (10-fold). ### CHANGE THIS TO BETTER SUIT OUR DATASET
 """
@@ -10,9 +10,17 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import KFold
 
-#-- load the data file --
+# -- load the data file --
+# Loads the CSV file/dataset
+data = pd.read_csv('student_spending.csv')  # 1000 rows x 18 columns
+data = data.rename(columns={'Unnamed: 0': 'Student'})
 
-#-- Preprocess the data as needed --
+# -- Preprocess the data as needed --
+# 'y' contains the unnamed number labels 0 - 999
+y = data.iloc[:, 0]  # 999 rows x 1 col
+
+# 'x' contains the features (label 'Student' is separated)
+x = data.drop('Student', axis=1)  # 1000 rows x 17 col
 
 # -- machine learning implementations --
 
