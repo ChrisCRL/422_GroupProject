22ffd62ac4f9299bfcdd612db2f771f1d090fb2e
Ethan Z <161177620+EthanZ23@users.noreply.github.com>
2024-04-24 14:01:10 -0700

    Update GroupProject.py

--- GroupProject.py
+++ GroupProject.py
@@ -2,17 +2,18 @@
  * CS 422 - 1002, Final Group Project
  * Group Member Names: Charles Ballesteros, Christopher Liscano, and
  *                     Ethan Zambrano
- * Input: .csv file: student_spending.csv
+ * Input: a .csv file: student_spending.csv
  * Output: Table showing coefficients of the seven independent variables
  *         and RMSE, for each fold (10-fold). ### CHANGE THIS TO BETTER SUIT OUR DATASET
 """
 import pandas as pd
+import numpy as np
 
 # -- load the data file --
 # Loads the CSV file/dataset
 data = pd.read_csv('student_spending.csv')  # 1000 rows x 18 columns
 data = data.rename(columns={'Unnamed: 0': 'Student'})
-print(data)
+# print(data)
 # -- Preprocess the data as needed --
 
 # change un-numerical data into numerical data in features: gender, year_in_school, major, preferred_payment_method
@@ -28,12 +29,14 @@ data['preferred_payment_method'] = data['preferred_payment_method'].map(dict_pre
 
 # 'y' contains the unnamed number labels 0 - 999
 y = data.iloc[:, 0]  # 999 rows x 1 col
+# 'X' contains the features (label 'Student' is separated)
+X = data.drop('Student', axis=1)  # 1000 rows x 17 col
 
-# 'x' contains the features (label 'Student' is separated)
-x = data.drop('Student', axis=1)  # 1000 rows x 17 col
+# -- machine learning implementations (KNN and linear regression)--
 
-# -- machine learning implementations --
 
-# -- main ? --
+# - use cross-validation to compare between KNN and linear reg -
+
+# -- main --
 
 # -- output section --
